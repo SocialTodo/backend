1224b35651e597e78bfceefbd0524a0439e1a20d
BrannenGHall <Brannen@HurtadoHall.me>
2017-12-24 21:16:44 -0600

    Set up login route

--- social_todo/urls.py
+++ social_todo/urls.py
@@ -2,5 +2,5 @@ from django.conf.urls import url
 import social_todo.views
 
 urlpatterns = [
-    url(r'^.*', social_todo.views.i_am_a_teapot_test)
+    url('login', social_todo.views.log_in_user)
 ]
--- social_todo/views.py
+++ social_todo/views.py
@@ -12,3 +12,8 @@ from django.shortcuts import render
 @csrf_exempt
 def i_am_a_teapot_test(request):
     return JsonResponse({"message": "ERROR 418: I'm sorry sir, but a poor little teapot like me is no use to you"}, status= 418)
+
+@csrf_exempt
+def log_in_user(request):
+    print(request.POST)
+    return HttpResponse(status=HTTPStatus.OK)
